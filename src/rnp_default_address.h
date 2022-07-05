1a0ce12b7ab834cdcf81edaf92621e07843278a7
kiran <kd619@ic.ac.uk>
2022-07-05 14:50:32 +0100

    updated default address

--- src/rnp_default_address.h
+++ src/rnp_default_address.h
@@ -6,6 +6,6 @@ enum class DEFAULT_ADDRESS:uint8_t{
     NOADDRESS = 0, //only route is debug on this address -> can ping this to get the current address
     DEBUG = 1,
     ROCKET = 2,
-    GROUNDSTATION = 3,
-    DESKTOP = 4
+    GROUNDSTATION_GATEWAY = 3,
+    GROUNDSTATION = 4
 };
