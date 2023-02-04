bf2f991c0ca18e04aee6f1a968c26f6f8cb2fb47
Max Hallgarten La Casta <34109153+maxhlc@users.noreply.github.com>
2023-02-04 12:18:14 +0000

    Packet documentation

--- src/rnp_packet.cpp
+++ src/rnp_packet.cpp
@@ -5,52 +5,68 @@
 
 RnpPacket::~RnpPacket(){};
 
-RnpPacket::RnpPacket(uint8_t packetService, uint8_t packetType, uint16_t packetSize) : header(packetService, packetType, packetSize){};
+RnpPacket::RnpPacket(const uint8_t packetService, const uint8_t packetType,
+                     const uint16_t packetSize)
+    : header(packetService, packetType, packetSize){};
 
-RnpPacket::RnpPacket(RnpHeader header) : header(header){};
+RnpPacket::RnpPacket(const RnpHeader header) : header(header){};
 
-RnpPacket::RnpPacket(const RnpPacketSerialized &serializedPacket, size_t size) : header(serializedPacket.header)
-{
-    
-    if (header.packet_len != size)
-    {
-        throw std::runtime_error("Header size does not match expeceted size!");
+RnpPacket::RnpPacket(const RnpPacketSerialized &serializedPacket, size_t size)
+    : header(serializedPacket.header) {
+    // Throw error if header size does not match expected size
+    if (header.packet_len != size) {
+        throw std::runtime_error("Header size does not match expected size!");
     }
-    if (serializedPacket.getBodySize() != size)
-    {
+
+    // Throw error if body size does not match expected size
+    if (serializedPacket.getBodySize() != size) {
         throw std::runtime_error("Buffer len does not match expected size!");
     }
 };
 
-void RnpPacket::serialize(std::vector<uint8_t>& buf){
+void RnpPacket::serialize(std::vector<uint8_t> &buf) {
+    // Serialize the header to the buffer
     header.serialize(buf);
 }
 
-RnpPacketSerialized::~RnpPacketSerialized()
-{};
+RnpPacketSerialized::~RnpPacketSerialized(){};
 
-RnpPacketSerialized::RnpPacketSerialized(const std::vector<uint8_t> &bytes):
-RnpPacket(RnpHeader(bytes)),
-packet(bytes)
-{};
+RnpPacketSerialized::RnpPacketSerialized(const std::vector<uint8_t> &bytes)
+    : RnpPacket(RnpHeader(bytes)), packet(bytes){};
 
-void RnpPacketSerialized::reserializeHeader(){
+void RnpPacketSerialized::reserializeHeader() {
+    // Declare serialized header bytes buffer
     std::vector<uint8_t> header_serialized;
+
+    // Serialize header to the buffer
     header.serialize(header_serialized);
-    std::memcpy(packet.data(),header_serialized.data(),header_serialized.size()); // replace original header in serialized packet
+
+    // Replace original header in the serialized packet
+    std::memcpy(packet.data(), header_serialized.data(),
+                header_serialized.size());
 }
 
-void RnpPacketSerialized::serialize(std::vector<uint8_t>& buf) {
+void RnpPacketSerialized::serialize(std::vector<uint8_t> &buf) {
+    // Re-serialize header
     reserializeHeader();
+
+    // Extract buffer size
     size_t bufsize = buf.size();
+
+    // Resize buffer to include the packet
     buf.resize(bufsize + packet.size());
-    std::memcpy(buf.data() + bufsize, packet.data(),packet.size());
+
+    // Copy the packet to the buffer
+    std::memcpy(buf.data() + bufsize, packet.data(), packet.size());
 };
 
-std::vector<uint8_t> RnpPacketSerialized::getBody()const { 
-    return std::vector<uint8_t> {packet.begin() + header.size(),packet.end()};
+std::vector<uint8_t> RnpPacketSerialized::getBody() const {
+    // Return the packet body
+    return std::vector<uint8_t>{packet.begin() + header.size(), packet.end()};
 }
 
-size_t RnpPacketSerialized::getBodySize()const {
-    return (packet.size() < header.size()) ? (0) : (packet.size() - header.size()); //ensure we dont return a negative number 
+size_t RnpPacketSerialized::getBodySize() const {
+    // Return the the size of the body, without returning a negative number
+    return (packet.size() < header.size()) ? (0)
+                                           : (packet.size() - header.size());
 }
