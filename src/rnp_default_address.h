15fb586f316cb909d8e5f39e35bb925fc74e0dba
Max Hallgarten La Casta <34109153+maxhlc@users.noreply.github.com>
2023-02-07 18:44:08 +0000

    Default address documentation

--- src/rnp_default_address.h
+++ src/rnp_default_address.h
@@ -2,10 +2,29 @@
 
 #include <stdint.h>
 
-enum class DEFAULT_ADDRESS:uint8_t{
-    NOADDRESS = 0, //only route is debug on this address -> can ping this to get the current address
+/**
+ * @brief Enumerate for default addresses
+ *
+ * @author Kiran de Silva
+ */
+enum class DEFAULT_ADDRESS : uint8_t {
+    /**
+     * @brief Null address
+     *
+     * Only route is debug on this address. This address can be pinged to get
+     * the current address.
+     */
+    NOADDRESS = 0,
+
+    /// @brief Debug address
     DEBUG = 1,
+
+    /// @brief Rocket address
     ROCKET = 2,
+
+    /// @brief Groundstation gateway address
     GROUNDSTATION_GATEWAY = 3,
-    GROUNDSTATION = 4
+
+    /// @brief Groundstation address
+    GROUNDSTATION = 4,
 };
