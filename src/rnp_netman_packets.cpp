c192bc0e36493b57984ca38ad8196cfbe13fa34e
Max Hallgarten La Casta <34109153+maxhlc@users.noreply.github.com>
2023-02-18 11:14:21 +0000

    Netman packets documentation

--- src/rnp_netman_packets.cpp
+++ src/rnp_netman_packets.cpp
@@ -1,89 +1,102 @@
 #include "rnp_netman_packets.h"
-#include "rnp_packet.h"
-#include "rnp_header.h"
-#include "rnp_serializer.h"
-#include "rnp_networkmanager.h"
-#include "rnp_routingtable.h"
 
 #include <cstring>
 
-SetRoutePacket::~SetRoutePacket()
-{};
+#include "rnp_header.h"
+#include "rnp_networkmanager.h"
+#include "rnp_packet.h"
+#include "rnp_routingtable.h"
+#include "rnp_serializer.h"
 
-SetRoutePacket::SetRoutePacket(uint8_t dest,Route& route):
-RnpPacket(static_cast<uint8_t>(DEFAULT_SERVICES::NETMAN),
-          static_cast<uint8_t>(NETMAN_TYPES::SET_ROUTE),
-          size()),
-destination(dest),
-iface(route.iface),
-metric(route.metric)
-{
-    switch(route.address.index()){
-        case 0:
-        {
-            //std::monostate -> no link layer addressing
-            address_type = (uint8_t)ADDRESS_TYPE::NOTYPE;
-            address_len = 0;
-            break;
-        }
-        case 1:
-        {
-            //std::string addressing
-            const std::string& address_string = std::get<std::string>(route.address);
-            address_type = (uint8_t)ADDRESS_TYPE::STRING;
-            address_len = address_string.size();
-            std::memcpy(address_data.data(),address_string.data(),address_len);
-            break;
-        }
-        default:
-        {
-            //unrecognized type
-            address_type = (uint8_t)ADDRESS_TYPE::NOTYPE;
-            address_len = 0;
-            break;
-        }
+SetRoutePacket::~SetRoutePacket(){};
+
+SetRoutePacket::SetRoutePacket(const uint8_t dest, const Route &route)
+    : RnpPacket(static_cast<uint8_t>(DEFAULT_SERVICES::NETMAN),
+                static_cast<uint8_t>(NETMAN_TYPES::SET_ROUTE), size()),
+      destination(dest), iface(route.iface), metric(route.metric) {
+    // Switch address type
+    switch (route.address.index()) {
+    case 0: { // No type
+        // Set address type and length
+        address_type = (uint8_t)ADDRESS_TYPE::NOTYPE;
+        address_len = 0;
+        break;
+    }
+    case 1: { // String
+        // set address string, type, and length
+        const std::string &address_string =
+            std::get<std::string>(route.address);
+        address_type = (uint8_t)ADDRESS_TYPE::STRING;
+        address_len = address_string.size();
+        std::memcpy(address_data.data(), address_string.data(), address_len);
+        break;
+    }
+    default: { // Unrecognised type
+        // Set address type and length
+        address_type = (uint8_t)ADDRESS_TYPE::NOTYPE;
+        address_len = 0;
+        break;
+    }
     }
 };
 
-SetRoutePacket::SetRoutePacket(RnpPacketSerialized& packet):
-RnpPacket(packet,size())
-{
-    getSerializer().deserialize(*this,packet.getBody());
-    const size_t offset = header.size() + getSerializer().member_size(); // the std::array is not included inthe serialziation framework 
-    std::memcpy(address_data.data(),packet.packet.data() + offset,address_len); // only copy what we neeed
+SetRoutePacket::SetRoutePacket(const RnpPacketSerialized &packet)
+    : RnpPacket(packet, size()) {
+    // Deserialize packet
+    getSerializer().deserialize(*this, packet.getBody());
+
+    // Calculate offset (not including the payload)
+    const size_t offset = header.size() + getSerializer().member_size();
+
+    // Copy the address data
+    std::memcpy(address_data.data(), packet.packet.data() + offset,
+                address_len);
 };
 
-void SetRoutePacket::serialize(std::vector<uint8_t> & buf){
+void SetRoutePacket::serialize(std::vector<uint8_t> &buf) {
+    // Serialize packet to buffer
     RnpPacket::serialize(buf);
+
+    // Extract buffer and member sizes
     const size_t bufsize = buf.size();
     const size_t member_size = getSerializer().member_size();
+
+    // Resize buffer
     buf.resize(bufsize + size());
-    //CAREFUL -> 2 seperate copy operations as address_Data is not included inthe serializer
-    std::memcpy(buf.data() + bufsize,
-                getSerializer().serialize(*this).data(),
+
+    // Copy data serialized by the serializer
+    std::memcpy(buf.data() + bufsize, getSerializer().serialize(*this).data(),
                 member_size);
-    std::memcpy(buf.data() + bufsize + member_size,
-                address_data.data(),
-                address_len); // only copy the necessary length -> rest of the elements in the vector are default constructed to zero
+
+    // Copy data not serialized by the serializer
+    std::memcpy(buf.data() + bufsize + member_size, address_data.data(),
+                address_len);
 }
 
-Route SetRoutePacket::getRoute() 
-{
-    Route ret{iface,metric};
-    switch(address_type){
-        case (uint8_t)ADDRESS_TYPE::NOTYPE:
-        {
-            return ret;
-        }
-        case (uint8_t)ADDRESS_TYPE::STRING:
-        {
-            std::string address(reinterpret_cast<char*>(address_data.data()),address_len); // reinterpret cast used here as underlying type of string is char* while uint8_t typedefs to unsigned char*
-            ret.address = address;
-            return ret;
-        }
-        default:
-        {
-            return ret;
-        }
+Route SetRoutePacket::getRoute() {
+    // Create route
+    Route ret{iface, metric};
+
+    // Switch based on address type
+    switch (address_type) {
+    case (uint8_t)ADDRESS_TYPE::NOTYPE: { // No type
+        // Return route
+        return ret;
+    }
+    case (uint8_t)ADDRESS_TYPE::STRING: { // String
+        // Create string from address data (unsigned char* -> char*)
+        std::string address(reinterpret_cast<char *>(address_data.data()),
+                            address_len);
+
+        // Update route address
+        ret.address = address;
+
+        // Return route
+        return ret;
+    }
+    default: { // Unrecognised type
+        // Return route
+        return ret;
+    }
     }
 };
--- src/rnp_netman_packets.h
+++ src/rnp_netman_packets.h
@@ -1,103 +1,211 @@
-#pragma once 
+#pragma once
 
-#include "rnp_packet.h"
 #include "rnp_header.h"
-#include "rnp_serializer.h"
-#include "rnp_routingtable.h"
 #include "rnp_networkmanager.h"
+#include "rnp_packet.h"
+#include "rnp_routingtable.h"
+#include "rnp_serializer.h"
 
 #include <array>
 
-
-enum class NETMAN_TYPES:uint8_t{
+/**
+ * @brief Enumerate for Network Manager Types
+ *
+ * @todo Save and Reset may benefit from a security key
+ *
+ * @author Kiran de Silva
+ */
+enum class NETMAN_TYPES : uint8_t {
+    /// @brief Ping request
     PING_REQ = 1,
+
+    /// @brief Ping result
     PING_RES = 2,
+
+    /// @brief Set address
     SET_ADDRESS = 3,
+
+    /// @brief Set route
     SET_ROUTE = 4,
+
+    /// @brief Set type
     SET_TYPE = 5,
+
+    /// @brief Set no route action
     SET_NOROUTEACTION = 6,
+
+    /// @brief Set route generation
     SET_ROUTEGEN = 7,
-    SAVE_CONF = 8, // this may benefit from a securtiy key
-    RESET_NETMAN = 9 // ^^
+
+    /// @brief Save Network Manager configuration
+    SAVE_CONF = 8,
+
+    /// @brief Reset Network Manager configuration
+    RESET_NETMAN = 9,
 
 };
 
+/**
+ * @brief Generic base packet
+ *
+ * @author Kiran de Silva
+ *
+ * @tparam TYPE Packet type
+ */
+template <uint8_t TYPE>
+using GenericRnpPacket_Base =
+    BasicDataPacket<uint32_t, TYPE,
+                    static_cast<uint8_t>(DEFAULT_SERVICES::NETMAN)>;
 
-template<uint8_t TYPE>
-using GenericRnpPacket_Base = BasicDataPacket<uint32_t,TYPE,static_cast<uint8_t>(DEFAULT_SERVICES::NETMAN)>;
+/**
+ * @brief Generic base packet (no type)
+ *
+ * @author Kiran de Silva
+ */
+using GenericRnpPacket =
+    GenericRnpPacket_Base<0>; // when we are reading data we dont care about the
+                              // type
 
-using GenericRnpPacket = GenericRnpPacket_Base<0>; //when we are reading data we dont care about the type 
+/**
+ * @brief Ping packet
+ *
+ * @author Kiran de Silva
+ */
+using PingPacket =
+    GenericRnpPacket_Base<static_cast<uint8_t>(NETMAN_TYPES::PING_REQ)>;
 
-using PingPacket = GenericRnpPacket_Base<static_cast<uint8_t>(NETMAN_TYPES::PING_REQ)>;
-using SetAddressPacket = GenericRnpPacket_Base<static_cast<uint8_t>(NETMAN_TYPES::SET_ADDRESS)>;
-using SetTypePacket = GenericRnpPacket_Base<static_cast<uint8_t>(NETMAN_TYPES::SET_TYPE)>;
-using SetNoRouteActionPacket = GenericRnpPacket_Base<static_cast<uint8_t>(NETMAN_TYPES::SET_NOROUTEACTION)>;
-using SetRouteGenPacket = GenericRnpPacket_Base<static_cast<uint8_t>(NETMAN_TYPES::SET_ROUTEGEN)>;
+/**
+ * @brief Set address packet
+ *
+ * @author Kiran de Silva
+ */
+using SetAddressPacket =
+    GenericRnpPacket_Base<static_cast<uint8_t>(NETMAN_TYPES::SET_ADDRESS)>;
 
+/**
+ * @brief Set type packet
+ *
+ * @author Kiran de Silva
+ */
+using SetTypePacket =
+    GenericRnpPacket_Base<static_cast<uint8_t>(NETMAN_TYPES::SET_TYPE)>;
 
-class SetRoutePacket: public RnpPacket{
-    enum class ADDRESS_TYPE{
-        NOTYPE = 0,
-        STRING = 1
+/**
+ * @brief Set no route action packet
+ *
+ * @author Kiran de Silva
+ */
+using SetNoRouteActionPacket = GenericRnpPacket_Base<static_cast<uint8_t>(
+    NETMAN_TYPES::SET_NOROUTEACTION)>;
+
+/**
+ * @brief Set route generation packet
+ *
+ * @author Kiran de Silva
+ */
+using SetRouteGenPacket =
+    GenericRnpPacket_Base<static_cast<uint8_t>(NETMAN_TYPES::SET_ROUTEGEN)>;
+
+/**
+ * @brief Packet class for setting routes
+ *
+ * @author Kiran de Silva
+ *
+ */
+class SetRoutePacket : public RnpPacket {
+    /// @brief Address type
+    enum class ADDRESS_TYPE { NOTYPE = 0, STRING = 1 };
+
+private:
+    /**
+     * @brief Get the packet Serializer
+     *
+     * @author Kiran de Silva
+     *
+     * @return constexpr auto Serializer
+     */
+    static constexpr auto getSerializer() {
+        auto ret = RnpSerializer(
+            &SetRoutePacket::destination, &SetRoutePacket::iface,
+            &SetRoutePacket::metric, &SetRoutePacket::address_type,
+            &SetRoutePacket::address_len);
+        return ret;
+    }
+
+public:
+    /**
+     * @brief Destroy the Set Route Packet object
+     *
+     * @author Kiran de Silva
+     */
+    ~SetRoutePacket();
+
+    /**
+     * @brief Create the Route Packet object
+     *
+     * @author Kiran de Silva
+     *
+     * @param[in] destination Destination
+     * @param[in] route Route
+     */
+    SetRoutePacket(const uint8_t destination, const Route &route);
+
+    /**
+     * @brief Create the Route Packet object
+     *
+     * @author Kiran de Silva
+     *
+     * @param[in] packet Serialized packet
+     */
+    SetRoutePacket(const RnpPacketSerialized &packet);
+
+    /**
+     * @brief Serialize Route Packet into buffer
+     *
+     * @author Kiran de Silva
+     *
+     * @param[out] buf Buffer
+     */
+    void serialize(std::vector<uint8_t> &buf) override;
+
+    /**
+     * @brief Get the Route from the Route Packet
+     *
+     * @author Kiran de Silva
+     *
+     * @return Route Route
+     */
+    Route getRoute();
+
+    // data members
+    /// @brief Destination address
+    uint8_t destination;
+
+    /// @brief Interface
+    uint8_t iface;
+
+    /// @brief Number of hops
+    uint8_t metric;
+
+    /// @brief Address type
+    uint8_t address_type;
+
+    /// @brief Address length
+    uint8_t address_len;
+
+    /// @brief Address data
+    std::array<uint8_t, 32> address_data;
+
+    /**
+     * @brief Get the size of the Set Route Packet
+     *
+     * @author Kiran de Silva
+     *
+     * @return constexpr size_t Route Packet size
+     */
+    static constexpr size_t size() {
+        // Return size of packet (including payload which is not in the
+        // serializer)
+        return getSerializer().member_size() + sizeof(address_data);
     };
-    private:
-        static constexpr auto getSerializer()
-        {
-            auto ret = RnpSerializer(
-                &SetRoutePacket::destination,
-                &SetRoutePacket::iface,
-                &SetRoutePacket::metric,
-                &SetRoutePacket::address_type,
-                &SetRoutePacket::address_len
-            );
-            return ret;
-        }
-    public:
-        ~SetRoutePacket();
-
-        /**
-         * @brief Construct a new Ping Packet object
-         * 
-         * @param sys_time 
-         */
-        SetRoutePacket(uint8_t destination,Route& route);
-
-        /**
-         * @brief Deserialize Ping Packet
-         * 
-         * @param packet 
-         */
-        SetRoutePacket(RnpPacketSerialized& packet);
-
-        /**
-         * @brief Serialize into buf
-         * 
-         * @param buf 
-         */
-        void serialize(std::vector<uint8_t>& buf) override;
-
-        /**
-         * @brief Returns the data in a route object for easier insertion into routing table
-         * 
-         * @return Route 
-         */
-        Route getRoute();
-
-        //data members
-        uint8_t destination;
-
-        uint8_t iface;
-        uint8_t metric;
-
-        uint8_t address_type;
-        uint8_t address_len;
-        
-        std::array<uint8_t,32> address_data; //address payload
-
-    
-        
-        static constexpr size_t size(){
-            return getSerializer().member_size() + sizeof(address_data);
-        };
 };
-
