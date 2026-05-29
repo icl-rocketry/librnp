56b4ae31e0414902747bb539a17175d22e96c3c5
Oliver M <oliver.mcnaughton@gmail.com>
2026-05-29 14:24:47 +0100

    Update RNP network manager for GNC throttle

--- src/librnp/rnp_networkmanager.cpp
+++ src/librnp/rnp_networkmanager.cpp
@@ -403,7 +403,6 @@ void RnpNetworkManager::routePackets() {
 
     // Send packet to capture callbacks
     for (PacketCaptureHandlerCb packetCapture : packetCaptureCallbacks) {
-		log("looping through packet handler callbacks");
         packetCapture(*packet_ptr);
     }
 
